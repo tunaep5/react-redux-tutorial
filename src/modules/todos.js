be24c6a15dc64b3639952c1a9b26ad111d56aa7d
sunyoung <sunnyk5780@gmail.com>
2021-01-03 22:51:07 +0900

    Add todos.js which is redux code for Todos component

---
+++ src/modules/todos.js
@@ -0,0 +1,83 @@
+// action types
+const CHANGE_INPUT = 'todo/CHANGE_INPUT';
+const INSERT = 'todos/INSERT';
+const TOGGLE = 'todos/TOGGLE';
+const REMOVE = 'todos/REMOVE';
+
+// actions
+export const chageInput = () => ({
+  type: CHANGE_INPUT,
+  input,
+});
+
+let id = 3;
+export const insert = (text) => ({
+  type: INSERT,
+  todo: {
+    id: id++,
+    text,
+    done: false,
+  },
+});
+
+export const toggle = (id) => ({
+  type: TOGGLE,
+  id,
+});
+
+export const remove = (id) => ({
+  type: REMOVE,
+  id,
+});
+
+// reducers
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: 'Study React',
+      done: true,
+    },
+    {
+      id: 2,
+      text: 'Study Redux',
+      done: false,
+    },
+  ],
+};
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case CHANGE_INPUT:
+      return {
+        ...state,
+        input: action.input,
+      };
+
+    case INSERT:
+      return {
+        ...state,
+        todos: state.todos.concat(actions.todo),
+      };
+
+    case TOGGLE:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.id ? { ...todo, done: !todo.done } : todo,
+        ),
+      };
+
+    case REMOVE:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.id),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default todos;
